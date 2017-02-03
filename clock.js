7f1ff91021fd4687aef61352708d8d62bc670a02
Romil Robtsenkov <romilrobtsenkov@users.noreply.github.com>
2017-02-03 09:25:29 +0200

    added clock example

---
+++ clock.js
@@ -0,0 +1,36 @@
+
+// aken laetud
+window.onload = function(){
+
+    var clockContainer = document.getElementById('clock');
+    console.log(clockContainer);
+
+    // kohe kirjutame ka kellaaja
+    clockContainer.innerHTML = getCurrentDateTime();
+
+    window.setInterval(function(){
+        clockContainer.innerHTML = getCurrentDateTime();
+    }, 1000);
+
+
+};
+
+var getCurrentDateTime = function(){
+    var currentDate = new Date();
+
+    var hours = currentDate.getHours();
+    var minutes = currentDate.getMinutes();
+    var seconds = currentDate.getSeconds();
+
+    var dateString = addZeroBefore(hours) + ':' + addZeroBefore(minutes) + ':' + addZeroBefore(seconds);
+
+    return dateString;
+};
+
+function addZeroBefore(dateNumber) {
+    if (dateNumber < 10) {
+        dateNumber = '0' + dateNumber;
+    }
+
+    return dateNumber;
+}
